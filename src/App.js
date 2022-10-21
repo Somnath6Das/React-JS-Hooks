7b0316948c071ad8f8f40fa2a3f7a62ffff2fbc1
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-10-22 01:42:00 +0530

    Context API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
-import User from "./components/User";
-
+import React, { useState } from "react";
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import { PostProvider } from "./context/PostContext";
 
 function App() {
+  
+
   return (
-    <>
-      <div className="container">
-             <User/>
+    <PostProvider>
+      <div className="App">
+        <h1>Navbar Section</h1>
+            <HomePage />
+        <h1>Footer Section</h1>       
       </div>
-    </>
+    </PostProvider>
+      
   );
 }
 
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Posts from "../components/Posts";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Posts />
+        </div>
+    )
+}
+
+
+export default HomePage;

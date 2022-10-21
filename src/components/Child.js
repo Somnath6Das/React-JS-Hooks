2a7939577ed6d6e8d4f849f19831a4c422fd9265
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-10-21 16:56:45 +0530

    useLogger

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,12 +1,15 @@
 import React,{forwardRef,useImperativeHandle, useState} from 'react'
+import useLogger from '../hooks/useLogger';
 
 
 const Child = forwardRef((props,ref) => {
    const [counter, setCounter] = useState(0);
 
+   useLogger(counter);
+
     useImperativeHandle(ref,() => ({
         handleCounterChange,
-    }))
+    }));
 
    const handleCounterChange = () => {
     setCounter(counter + 1);
@@ -18,6 +21,6 @@ const Child = forwardRef((props,ref) => {
             Increment Child</button>
     </div>
   )
-}) 
+});
 
 export default Child
